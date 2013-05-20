ffdab3c58b2eaef983f3149809c573ef9f24d2a0
cstella <cestella@gmail.com>
2013-05-20 14:31:09 -0400

    Initial Commit

---
+++ src/main/java/com/caseystella/talks/App.java
@@ -0,0 +1,13 @@
+package com.caseystella.talks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
