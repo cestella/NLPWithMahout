fc9de2dcd9a9ac4b3209b1603aa74244e28e0c49
cstella <cestella@gmail.com>
2013-06-06 21:41:44 -0400

    Removing data.

--- src/main/java/com/caseystella/interfaces/IBackingStore.java
+++
@@ -1,9 +0,0 @@
-package com.caseystella.interfaces;
-
-import com.caseystella.KNN.Payload;
-
-public interface IBackingStore
-   {
-      public void persist(long key, Payload payload);
-      public Iterable<Payload> getBucket(long key);
-   }
--- src/main/java/com/caseystella/interfaces/IDistanceMetric.java
+++
@@ -1,8 +0,0 @@
-package com.caseystella.interfaces;
-
-import org.apache.commons.math.linear.RealVector;
-
-public interface IDistanceMetric
-   {
-	   public double apply(RealVector v1, RealVector v2);
-   }
